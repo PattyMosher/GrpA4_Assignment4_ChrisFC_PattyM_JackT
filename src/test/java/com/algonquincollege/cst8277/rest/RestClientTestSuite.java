9cde4a75fa4c6bcfde297bd304ca77c3216a5bcf
Jackshares <jack.wen.tan@gmail.com>
2019-11-30 11:43:33 -0500

    Revert "Task 3 WIP"
    This reverts commit 317e7a87bb561c5ecca73702f96ff527afb75515.

--- src/test/java/com/algonquincollege/cst8277/rest/RestClientTestSuite.java
+++ src/test/java/com/algonquincollege/cst8277/rest/RestClientTestSuite.java
@@ -22,11 +22,8 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
-import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.algonquincollege.cst8277.models.PlatformRole;
 
 public class RestClientTestSuite  {
 
@@ -45,36 +42,36 @@ public class RestClientTestSuite  {
                 .port(8080);
         webTarget = client.target(uriBuilder);
     }
-    
-    @Test
-    public void test_get_accounts() {
-        Response response = webTarget
-                .path("account/1")
-                .request(APPLICATION_JSON)
-                .get();
-        assertThat(response.getStatus(), is(200));
-//        PlatformRole platformRole = response.readEntity(PlatformRole.class);
-//        assertEquals("hello from the other side!", platformRole.getRoleName());
-    }
-    
-    @Test
-    public void testBasicAuth() {
-        HttpAuthenticationFeature feature = 
-            HttpAuthenticationFeature.basic("admin", "admin");
-        Client client = ClientBuilder.newClient();
-        URI uri = UriBuilder
-            .fromUri(APPLICATION_NAME + APPLICATION_API_VERSION)
-            .scheme("http")
-            .host("localhost")
-            .port(8080)
-            .build();
-        WebTarget webTarget = client
-            .register(feature)
-            .target(uri)
-            .path("user");
-        Response response = webTarget
-            .request(APPLICATION_JSON)
-            .get();
-        assertThat(response.getStatus(), is(200));
-    }
+//    
+//    @Test
+//    public void test_hello_get() {
+//        Response response = webTarget
+//                .path("hello")
+//                .request(APPLICATION_JSON)
+//                .get();
+//        assertThat(response.getStatus(), is(200));
+//        MessageHolder msgH = response.readEntity(MessageHolder.class);
+//        assertEquals("hello from the other side!", msgH.getMsg());
+//    }
+//    
+//    @Test
+//    public void testBasicAuth() {
+//        HttpAuthenticationFeature feature = 
+//            HttpAuthenticationFeature.basic("admin", "admin");
+//        Client client = ClientBuilder.newClient();
+//        URI uri = UriBuilder
+//            .fromUri(APPLICATION_NAME + APPLICATION_API_VERSION)
+//            .scheme("http")
+//            .host("localhost")
+//            .port(8080)
+//            .build();
+//        WebTarget webTarget = client
+//            .register(feature)
+//            .target(uri)
+//            .path("user");
+//        Response response = webTarget
+//            .request(APPLICATION_JSON)
+//            .get();
+//        assertThat(response.getStatus(), is(200));
+//    }
 }
